002607956e3bf300352fe2e947968c8f159aeb83
Twaites <kory@twaites.com>
2025-01-22 20:23:30 -0800

    fixing path casing

--- src/Telestrator.tsx
+++ src/Telestrator.tsx
@@ -1,8 +1,8 @@
 import './Telestrator.css'
-import VideoPlayer from './components/VideoPlayer.tsx'
-import SketchCanvas from './components/SketchCanvas.tsx'
-import SketchToolbar from './components/SketchToolbar.tsx'
-import VideoControls from './components/VideoControls.tsx'
+import VideoPlayer from './components2/VideoPlayer.tsx'
+import SketchCanvas from './components2/SketchCanvas.tsx'
+import SketchToolbar from './components2/SketchToolbar.tsx'
+import VideoControls from './components2/VideoControls.tsx'
 
 function Telestrator() {
   return (
